cee8041082ad693d695ba3340d3a017f74cb6260
Aegean <aegeanthompson@gmail.com>
2019-09-06 17:11:28 -0500

    merge conflict fix

--- overture_portfolios/config/routes.rb
+++ overture_portfolios/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root 'site#index'
+ # root route
+ root 'site#index'
+ # posts routes
+ get 'api/posts', to: 'posts#index'
+ get 'api/posts/:id', to: 'posts#show'
+ post 'api/posts', to: 'posts#create'
+ delete 'api/posts/:id', to: 'posts#delete'
+ put 'api/posts/:id', to: 'posts#update'
 end
